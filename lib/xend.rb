f30f8028013cf4d999dd887ceac372ea82b45c5d
Luis Vega <luiscvega@gmail.com>
2012-08-26 13:40:52 +0800

    Added back calculate for comparison.

--- lib/xend.rb
+++ lib/xend.rb
@@ -2,7 +2,10 @@ require 'ap'
 require 'open3'
 require 'xmlsimple'
 
-stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -v -d @schedule.xml https://www.xend.com.ph/apitest/BookingService.asmx')
+# This works
+stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @calculate.xml https://www.xend.com.ph/api/RateService.asmx')
 
+# This doesn't (unauthorized caller, might be wrong sandbox user token?)
+stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @schedule.xml https://www.xend.com.ph/apitest/BookingService.asmx')
 
 ap XmlSimple.xml_in(stdout)
