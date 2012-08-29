3d2296460aa4d9f9b335cba36e2886a9ad7a9fe4
Luis Vega <luiscvega@gmail.com>
2012-08-29 23:54:29 +0800

    Added other API methods. Everything works! Next step: create methods / automate

--- lib/xend.rb
+++ lib/xend.rb
@@ -2,10 +2,30 @@ require 'ap'
 require 'open3'
 require 'xmlsimple'
 
-# This works
-stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @calculate.xml https://www.xend.com.ph/api/RateService.asmx')
+# SANDBOX
+  # RateService
+# stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @rate-calculate-sandbox.xml https://www.xend.com.ph/api/RateService.asmx')
 
-# This doesn't (unauthorized caller, might be wrong sandbox user token?)
-stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @schedule.xml https://www.xend.com.ph/apitest/BookingService.asmx')
+  # BookingService
+# stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @booking-schedule-sandbox.xml https://www.xend.com.ph/apitest/BookingService.asmx')
+
+  # ShipmentService
+
+    # Create
+# stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @shipment-create-sandbox.xml https://www.xend.com.ph/apitest/ShipmentService.asmx')
+
+  # Get
+# stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @shipment-get-sandbox.xml https://www.xend.com.ph/apitest/ShipmentService.asmx')
+
+
+
+# LIVE
+
+  # ShipmentService
+    # Create
+# stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @shipment-create.xml https://www.xend.com.ph/api/ShipmentService.asmx')
+
+    # Get
+stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @shipment-get.xml https://www.xend.com.ph/api/ShipmentService.asmx')
 
 ap XmlSimple.xml_in(stdout)
