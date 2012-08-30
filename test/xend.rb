51b98a88ece56b792f6d177cf5e6f5aa70742081
Luis Vega <luiscvega@gmail.com>
2012-08-30 21:04:48 +0800

    Removed some xml files and added optional declared value for rate calculator

--- test/xend.rb
+++ test/xend.rb
@@ -2,17 +2,20 @@ require_relative "helper"
 
 # RateService
 test "calculate small" do
-  rate = Xend::Rate.calculate(weight: 1, length: 10, width: 10, height: 10)
+  rate = Xend::Rate.calculate(type: Xend::MetroManila,
+                              weight: 1, length: 10, width: 10, height: 10)
   assert_equal "64.960000", rate
 end
 
 test "calculate medium" do
-  rate = Xend::Rate.calculate(weight: 1, length: 50, width: 50, height: 50)
+  rate = Xend::Rate.calculate(type: Xend::MetroManila,
+                              weight: 1, length: 50, width: 50, height: 50)
   assert_equal "928.480000", rate
 end
 
 test "calculate big" do
-  rate = Xend::Rate.calculate(weight: 1, length: 1000, width: 1000, height: 1000)
+  rate = Xend::Rate.calculate(type: Xend::MetroManila,
+                              weight: 1, length: 1000, width: 1000, height: 1000)
   assert_equal nil, rate
 end
 
