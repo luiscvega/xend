82616f83af88cb31a3331416dbdf7f3f735f3f9a
Luis Vega <luiscvega@gmail.com>
2012-08-30 12:40:42 +0800

    Added ShipmentService.get method to retrieve waybill info

--- lib/xend.rb
+++ lib/xend.rb
@@ -3,25 +3,28 @@ require 'open3'
 require 'xmlsimple'
 
 class Xend
-  def self.curl(soap)
-    Open3.capture3('curl -H "Content-Type: text/xml" -d @../templates/rate-calculate.xml %s' % soap)
-  end
-
-  def self.xml(raw_xml)
-    XmlSimple.xml_in(raw_xml)
+  def self.request(file, soap)
+    Open3.capture3('curl -H "Content-Type: text/xml" -d @../templates/%s.xml %s' % [file, soap])
   end
 
   class Rate < Xend
     SOAP = "https://www.xend.com.ph/api/RateService.asmx"
 
     def self.calculate
-      stdout, stderr, res = curl(SOAP)
-
-      response = xml stdout
-
-      rate = response["Body"][0]["CalculateResponse"][0]["CalculateResult"][0]
-
+      stdout, stderr, res = request("rate-calculate", SOAP)
+      XmlSimple.xml_in(stdout)["Body"][0]["CalculateResponse"][0]["CalculateResult"][0]
     end
+  end
+
+  class Shipment < Xend
+    SOAP = "https://www.xend.com.ph/api/ShipmentService.asmx"
+
+    def self.get
+      stdout, stderr, res = request("shipment-get", SOAP)
+
+      XmlSimple.xml_in(stdout)
+    end
+
   end
 
 end
--- test/xend.rb
+++ test/xend.rb
@@ -1,7 +1,13 @@
 require_relative "helper"
 
-# RateCalculator
+# RateService
 test "calculate" do
   rate = Xend::Rate.calculate
   assert_equal "64.960000", rate
+end
+
+# ShipmentService
+test "get" do
+  waybill = Xend::Shipment.get
+  assert_equal "733603482", waybill["Body"][0]["GetResponse"][0]["GetResult"][0]["WayBillNo"][0]
 end
