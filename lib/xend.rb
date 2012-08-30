09ddf1274283af83c787e322de93435f95f1a591
Luis Vega <luiscvega@gmail.com>
2012-08-30 14:16:32 +0800

    Added response object

--- lib/xend.rb
+++ lib/xend.rb
@@ -5,16 +5,17 @@ require 'xmlsimple'
 class Xend
   TEMPLATES = File.expand_path("../templates", File.dirname(__FILE__))
 
-  def self.request(file, soap)
-    Open3.capture3('curl -H "Content-Type: text/xml" -d @%s %s' % [File.join(TEMPLATES, "%s.xml" % file), soap])
-  end
+  def self.post(file, soap)
+    stdout, stderr, res = Open3.capture3('curl -H "Content-Type: text/xml" -d @%s %s' % [File.join(TEMPLATES, "%s.xml" % file), soap])
 
+    return stdout
+  end
+  
   class Rate < Xend
     SOAP = "https://www.xend.com.ph/api/RateService.asmx"
 
     def self.calculate
-      stdout, stderr, res = request("rate-calculate", SOAP)
-      XmlSimple.xml_in(stdout)["Body"][0]["CalculateResponse"][0]["CalculateResult"][0]
+      Response.new(post("rate-calculate", SOAP))
     end
   end
 
@@ -22,11 +23,22 @@ class Xend
     SOAP = "https://www.xend.com.ph/api/ShipmentService.asmx"
 
     def self.get
-      stdout, stderr, res = request("shipment-get", SOAP)
-
-      XmlSimple.xml_in(stdout)
+      Response.new(post("shipment-get", SOAP))
     end
 
   end
 
+  class Response
+    def initialize(xml)
+      @data = XmlSimple.xml_in(xml)
+    end
+
+    def rate
+      @data["Body"][0]["CalculateResponse"][0]["CalculateResult"][0]
+    end
+
+    def waybill
+      @data["Body"][0]["GetResponse"][0]["GetResult"][0]["WayBillNo"][0]
+    end
+  end
 end
--- test/xend.rb
+++ test/xend.rb
@@ -2,12 +2,14 @@ require_relative "helper"
 
 # RateService
 test "calculate" do
-  rate = Xend::Rate.calculate
+  response = Xend::Rate.calculate
+  rate = response.rate
   assert_equal "64.960000", rate
 end
 
 # ShipmentService
 test "get" do
-  waybill = Xend::Shipment.get
-  assert_equal "733603482", waybill["Body"][0]["GetResponse"][0]["GetResult"][0]["WayBillNo"][0]
+  response = Xend::Shipment.get
+  waybill = response.waybill
+  assert_equal "733603482", waybill
 end
